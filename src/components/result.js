b76b98c53e0488d241cb0de476b47ec2720392d8
Glen Honeybone <glen@urgedesign.co.nz>
2018-08-27 15:24:27 +1200

    Updated font

--- src/components/result.js
+++ src/components/result.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Result extends Component {
     message() {
         if (this.props.value === null) {
-            return <span className="c-result  c-result--empty">throw to begin</span>;
+            return <span className="c-result  c-result--empty">Choose a throw to start</span>;
         } else {
             return <span className="c-result">{this.props.value}</span>;
         }
